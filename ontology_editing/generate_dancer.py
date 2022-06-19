f8f4e9d0a78f7e6ee78b5cf9d911f69823cfaa5a
rounaksaha12 <rounaksaha12@gmail.com>
2022-06-19 13:03:16 +0530

    dancer,costume instance addition, image instance creation

--- ontology_editing/generate_dancer.py
+++ ontology_editing/generate_dancer.py
@@ -1,8 +1,18 @@
-
 f=open('dancer_list.tsv','w')
 f.write('Dancer\tName\tAge\tGender\n')
-
 for i in range(10):
-	f.write('Dancer'+str(i+3)+'\td'+str(i+3)+'\t'+str(20+i)+'\t'+'F'+'\n')
+	if i%2:
+		G='M'
+	else:
+		G='F'
+	f.write('Dancer'+str(i)+'\td'+str(i)+'\t'+str(20+i)+'\t'+G+'\n')
 	
 f.close()
+
+f=open('costume_list.tsv','w')
+f.write('Costume\n')
+for i in range(5):
+	f.write('Costume'+str(i)+'\n')
+f.close
+
+
